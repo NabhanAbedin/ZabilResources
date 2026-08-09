ff6924fadc0f42c6eedc2e1690fba4a46f5548aa
Nabhan <nabedi2006@gmail.com>
2026-08-09 18:35:58 -0400

    add

--- backend/Zabil.Tests/obj/Debug/net9.0/Zabil.Tests.AssemblyInfo.cs
+++ backend/Zabil.Tests/obj/Debug/net9.0/Zabil.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e494dd97dad9463cc5bd2bf5994697b5fe31f8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842052c9b6af12f5bff3124d0cb32444cc82b5f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
