8f5c85b62e8afd26ecf279b6c06a3b47022638cc
Nabhan <nabedi2006@gmail.com>
2026-08-11 22:55:17 -0400

    added

--- backend/Zabil.Api/obj/Debug/net9.0/Zabil.Api.AssemblyInfo.cs
+++ backend/Zabil.Api/obj/Debug/net9.0/Zabil.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zabil.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e494dd97dad9463cc5bd2bf5994697b5fe31f8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6924fadc0f42c6eedc2e1690fba4a46f5548aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zabil.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zabil.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Zabil.Tests/obj/Debug/net9.0/Zabil.Tests.AssemblyInfo.cs
+++ backend/Zabil.Tests/obj/Debug/net9.0/Zabil.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842052c9b6af12f5bff3124d0cb32444cc82b5f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6924fadc0f42c6eedc2e1690fba4a46f5548aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zabil.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
